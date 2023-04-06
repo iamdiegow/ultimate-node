d56bbb48a424408467a4b1b7718c97254938a3a5
Diego Weinmann <diegoweinmann@gmail.com>
2023-04-06 01:25:14 -0300

    build: add dockerfile and docker-compose

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world!');
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (_req, res) => {
+	res.json({
+		hello: 'world',
+	});
+});
+
+const PORT = Number(process.env.PORT || 3000);
+
+app.listen(PORT, '0.0.0.0', () => {
+	console.log(`Server listening at ${PORT}`);
+});
